89edc264783c228f1d26e536106bb291eea45018
totto357 <hosozawn@B242480.waterblue.justsystem.co.jp>
2019-08-07 17:59:05 +0900

    ライブ機能に必要なものを追加

--- examples/pagemirror_extension/content_script.js
+++ examples/pagemirror_extension/content_script.js
@@ -1,45 +1,168 @@
-// Copyright 2011 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-var observer;
-
-if (typeof WebKitMutationObserver != 'function') {
-  console.error('This example extension requires MutationObservers. ' +
-                'Try the Chrome Canary build.');
-  return;
+const uuid = () => {
+  let chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
+  for (let i = 0, len = chars.length; i < len; i++) {
+    switch (chars[i]) {
+      case "x":
+        chars[i] = Math.floor(Math.random() * 16).toString(16);
+        break;
+      case "y":
+        chars[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+        break;
+    }
+  }
+  return chars.join("");
 }
 
-chrome.extension.onConnect.addListener(function(port) {
-  port.postMessage({ base: location.href.match(/^(.*\/)[^\/]*$/)[1] });
+// 変数
+const sessionId = uuid()
 
-  var mirrorClient = new TreeMirrorClient(document, {
-    initialize: function(rootId, children) {
-      port.postMessage({
-        f: 'initialize',
+const throttleInterval = 50
+const lastTime = {
+  scroll: 0,
+  mousemove: 0,
+  resize: 0,
+}
+
+let eventListeners = []
+
+chrome.extension.onConnect.addListener(function (port) {
+
+  const throttle = (type, cb) => {
+    const now = new Date().getTime()
+    if (lastTime[type] + throttleInterval <= now) {
+      lastTime[type] = now
+      cb()
+    }
+  }
+
+  const postMessage = ({ type, args }) => {
+    // 別タブにメッセージを送信
+    let message = {
+      type,
+      time: new Date().getTime(),
+      sessionId,
+      eventId: uuid(),
+      divided: false,
+      args: null,
+    }
+
+    // データを1MBを目安に分割
+    const divided = divide(args)
+
+    // 分割がない場合
+    if (divided.length === 1) {
+      message.args = divided[0]
+      port.postMessage(message)
+      return
+    }
+
+    // 分割する場合
+    for (let index = 0; index < divided.length; index++) {
+      message.divided = {
+        index,
+        size: divided.length,
+      }
+      message.args = divided[index]
+      port.postMessage(message)
+    }
+
+  }
+
+  const divide = (args) => {
+    // エンコード済みのargs
+    // 1MB ずつ分割
+    return JSON.stringify(args).match(/[\s\S]{1,500000}/g)
+  }
+
+  // 開始イベント
+  postMessage({
+    type: "start",
+    args: {
+      base: location.href.match(/^(.*\/)[^\/]*$/)[1],
+    }
+  });
+
+  // TreeMirrorからのイベント
+  const mirrorClient = new TreeMirrorClient(document, {
+    initialize: function (rootId, children) {
+      postMessage({
+        type: 'initialize',
         args: [rootId, children]
       });
     },
 
-    applyChanged: function(removed, addedOrMoved, attributes, text) {
-      port.postMessage({
-        f: 'applyChanged',
+    applyChanged: function (removed, addedOrMoved, attributes, text) {
+      postMessage({
+        type: 'applyChanged',
         args: [removed, addedOrMoved, attributes, text]
       });
     }
   });
 
-  port.onDisconnect.addListener(function() {
+  // 独自のイベント
+  // スクロール
+  const scrollEventListener = (e) => {
+    // スクロールの対象がHTMLの場合、
+    // スクロールイベントのターゲットはdocumentになる(chromeだけ？)
+    // しかし、mirror側ではbodyがスクロールイベントにする必要があるため、(iframeの仕様？)
+    // HTMLのスクロールかどうかを渡してあげる必要がある
+    const target = e.target === document ? document.scrollingElement : e.target
+    const isScrollingElement = target === document.scrollingElement
+    const node = mirrorClient.serializeNode(target)
+    if (!node) {
+      return
+    }
+
+    throttle("scroll", () => {
+      postMessage({
+        type: 'scroll',
+        args: {
+          target: node.id,
+          isScrollingElement,
+          top: target.scrollTop,
+          left: target.scrollLeft,
+        }
+      })
+    })
+  }
+  window.addEventListener("scroll", scrollEventListener, true)
+  eventListeners.push(["scroll", scrollEventListener, true])
+
+  // マウス
+  const mousemoveEventListener = (e) => {
+    throttle("mousemove", () => {
+      postMessage({
+        type: 'mousemove',
+        args: {
+          clientX: e.clientX,
+          clientY: e.clientY,
+        }
+      })
+    })
+  }
+  window.addEventListener("mousemove", mousemoveEventListener)
+  eventListeners.push(["mousemove", mousemoveEventListener])
+
+  // リサイズ
+  const resizeEventListener = () => {
+    throttle("resize", () => {
+      postMessage({
+        type: 'resize',
+        args: {
+          height: window.innerHeight,
+          width: window.innerWidth,
+        }
+      })
+    });
+  }
+  resizeEventListener()
+  window.addEventListener("resize", resizeEventListener)
+  eventListeners.push(["resize", resizeEventListener])
+
+  port.onDisconnect.addListener(function () {
     mirrorClient.disconnect();
+    eventListeners.forEach(el => window.removeEventListener.apply(window, el))
+    eventListeners = []
   });
+
 });
--- examples/pagemirror_extension/mirror.js
+++ examples/pagemirror_extension/mirror.js
@@ -1,18 +1,5 @@
-// Copyright 2012 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   if (typeof WebKitMutationObserver !== 'function') {
     var h1 = document.body.appendChild(document.createElement('h3'));
     h1.textContent = 'This example requires Mutation Observers';
@@ -24,42 +11,131 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   var tabId = Number(location.href.match(/\?tabId=([0-9]*$)/)[1]);
-  if (isNaN(tabId))
+  if (isNaN(tabId)) {
     return;
-
-  while (document.firstChild) {
-    document.removeChild(document.firstChild);
   }
 
-  var base;
+  const frame = document.getElementById("mirror-frame")
+  if (!frame) {
+    return;
+  }
+  const frameDocument = frame.contentDocument
 
-  var mirror = new TreeMirror(document, {
-    createElement: function(tagName) {
+  while (frameDocument.firstChild) {
+    frameDocument.removeChild(frameDocument.firstChild);
+  }
+
+  let base = "";
+
+  var mirror = new TreeMirror(frameDocument, {
+    createElement: function (tagName) {
+      /* Somewhere outside the function (no need to recreate this every time an element is created */
+
+      // SVG関係
+      // TODO useタグで宣言されているものは難しい
+      const svgElems = ["altGlyph", "altGlyphDef", "altGlyphItem", "animate", "animateColor", "animateMotion", "animateTransform", "circle", "clipPath", "color-profile", "cursor", "defs", "desc", "discard", "ellipse", "feBlend", "feColorMatrix", "feComponentTransfer", "feComposite", "feConvolveMatrix", "feDiffuseLighting", "feDisplacementMap", "feDistantLight", "feDropShadow", "feFlood", "feFuncA", "feFuncB", "feFuncG", "feFuncR", "feGaussianBlur", "feImage", "feMerge", "feMergeNode", "feMorphology", "feOffset", "fePointLight", "feSpecularLighting", "feSpotLight", "feTile", "feTurbulence", "filter", "font", "font-face", "font-face-format", "font-face-name", "font-face-src", "font-face-uri", "foreignObject", "g", "glyph", "glyphRef", "hatch", "hatchpath", "hkern", "image", "line", "linearGradient", "marker", "mask", "mesh", "meshgradient", "meshpatch", "meshrow", "metadata", "missing-glyph", "mpath", "path", "pattern", "polygon", "polyline", "radialGradient", "rect", "set", "solidcolor", "stop", "svg", "use", "switch", "symbol", "text", "textPath", "tref", "tspan", "unknown", "view", "vkern"].join("|")
+      const svgMatch = new RegExp(`^(${svgElems})$`)
+
+      if (svgMatch.test(tagName)) {
+        const node = frameDocument.createElementNS(
+          "http://www.w3.org/2000/svg",
+          tagName
+        )
+        return node
+      }
+
       if (tagName == 'SCRIPT') {
-        var node = document.createElement('NO-SCRIPT');
-        node.style.display = 'none';
-        return node;
+        var node = frameDocument.createElement('NO-SCRIPT')
+        node.style.display = 'none'
+        return node
       }
 
       if (tagName == 'HEAD') {
-        var node = document.createElement('HEAD');
-        node.appendChild(document.createElement('BASE'));
-        node.firstChild.href = base;
-        return node;
+        var node = frameDocument.createElement('HEAD')
+        node.appendChild(frameDocument.createElement('BASE'))
+        node.firstChild.href = base
+        return node
       }
     }
   });
 
-  var port = chrome.tabs.connect(tabId);
+  let mouse = frameDocument.createElement("div")
+  mouse.innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                      <path fill="currentColor" d="M13.64,21.97C13.14,22.21 12.54,22 12.31,21.5L10.13,16.76L7.62,18.78C7.45,18.92 7.24,19 7,19A1,1 0 0,1 6,18V3A1,1 0 0,1 7,2C7.24,2 7.47,2.09 7.64,2.23L7.65,2.22L19.14,11.86C19.57,12.22 19.62,12.85 19.27,13.27C19.12,13.45 18.91,13.57 18.7,13.61L15.54,14.23L17.74,18.96C18,19.46 17.76,20.05 17.26,20.28L13.64,21.97Z" />
+                    </svg>`
+  mouse.style = `position: fixed;
+                 z-index: 99999999;
+                 top: -100px; left: -100px;
+                 color: #999;
+                `
 
-  port.onMessage.addListener(function(msg) {
-    if (msg.base)
-      base = msg.base;
-    else
-      mirror[msg.f].apply(mirror, msg.args);
-  });
+  const port = chrome.tabs.connect(tabId)
+  let events = {}
+  const receivedMessages = []
 
-  port.onDisconnect.addListener(function(msg) {
-    window.close();
-  });
-});
+  port.onMessage.addListener(function (msg) {
+    receivedMessages.push(msg)
+
+    let args = null
+    if (msg.divided) {
+      !events[msg.eventId] && (events[msg.eventId] = [])
+      events[msg.eventId].push({ index: msg.divided.index, args: msg.args })
+
+      // 分割してるイベントの途中はなにもしない
+      if (events[msg.eventId].length < msg.divided.size) {
+        return
+      }
+
+      const _args = events[msg.eventId].sort((a, b) => {
+        if (a.index < b.index) return -1
+        if (a.index > b.index) return 1
+        return 0
+      }).map(e => e.args).join("")
+
+      args = JSON.parse(_args)
+      delete events[msg.eventId]
+
+    } else {
+      args = JSON.parse(msg.args)
+    }
+
+    if (msg.type === "start") {
+      base = args.base;
+
+    } else if (msg.type === "initialize") {
+      mirror.initialize.apply(mirror, args);
+      frameDocument.body.appendChild(mouse)
+
+    } else if (msg.type === "applyChanged") {
+      mirror.applyChanged.apply(mirror, args);
+
+    } else if (msg.type === "scroll") {
+      const id = args.target
+      const node = args.isScrollingElement ? frameDocument.scrollingElement : mirror.idMap[id]
+      node.scrollTo(args.left, args.top)
+
+    } else if (msg.type === "mousemove") {
+      mouse.style.top = args.clientY
+      mouse.style.left = args.clientX
+
+    } else if (msg.type === "resize") {
+      frame.style.height = args.height
+      frame.style.width = args.width
+
+    } else {
+      console.error("対応するイベントタイプがないよ。")
+    }
+
+  })
+
+  port.onDisconnect.addListener(function (msg) {
+    const blob = new Blob([ JSON.stringify(receivedMessages) ], { "type" : "text/plain" })
+    window.URL = window.URL || window.webkitURL
+    const a = document.createElement("a")
+    a.textContent = "ダウンロード"
+    a.href = window.URL.createObjectURL(blob)
+    document.body.appendChild(a)
+
+    // window.close()
+  })
+})
